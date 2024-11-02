2f86ba4f6ec285649a7e8c00834d87711d338016
fatima <fatimajamil453@gmail.com>
2024-11-02 12:03:57 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,19 @@
-echo " BUILD START"
+#!/bin/bash
 
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+echo "BUILD START"
 
-echo " BUILD END"
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput --clear
+
+# Check if staticfiles_build is created
+if [ -d "staticfiles_build" ]; then
+    echo "staticfiles_build directory exists."
+else
+    echo "staticfiles_build directory does not exist."
+    exit 1  # Exit with an error if the directory does not exist
+fi
+
+echo "BUILD END"
