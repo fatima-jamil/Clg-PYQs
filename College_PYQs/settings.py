3a4d0a6a8de78f3481e1900fa46e17153603ba73
fatima <fatimajamil453@gmail.com>
2024-11-02 17:53:27 +0530

    login signup fixed

--- College_PYQs/settings.py
+++ College_PYQs/settings.py
@@ -126,3 +126,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- College_PYQs/urls.py
+++ College_PYQs/urls.py
@@ -20,6 +20,7 @@ from pyqs.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name = "index"),
